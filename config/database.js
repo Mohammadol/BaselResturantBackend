ddc63875c8ba9fd9e05381b8b6fc8807b03cb498
Mohammadol <mohammad-996-@hotmail.com>
2024-10-19 15:55:38 +0400

    remocing the password

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('baseldatabase', 'root', 'Mohammad_161281', {
+const sequelize = new Sequelize('baseldatabase', 'root', '', {
     host: '127.0.0.1',
     dialect: 'mysql' // or 'postgres', 'sqlite', etc.
 });
